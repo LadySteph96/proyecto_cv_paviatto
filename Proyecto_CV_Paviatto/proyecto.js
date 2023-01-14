aa826a4a27e5c796823ee3e7c654189613af7a46
LadySteph96 <122689379+LadySteph96@users.noreply.github.com>
2023-01-14 16:48:02 -0300

    Add files via upload

---
+++ Proyecto_CV_Paviatto/proyecto.js
@@ -0,0 +1,10 @@
+function mostrar_form() {
+document.getElementById('contact_form').style ="display: block";
+document.getElementById('contactame').style ="display: none";
+}
+
+function ocultar_form() {
+    document.getElementById('contact_form').style ="display: none";
+    document.getElementById('contactame').style ="display: block";
+}
+
